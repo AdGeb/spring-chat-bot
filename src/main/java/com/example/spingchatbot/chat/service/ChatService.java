e35e0c89ea2ee1d81c2550fd84a33e5b2fb3c8c2
Adam <agebura57@gmail.com>
2025-03-16 12:20:04 +0100

    Add chat bot history

--- src/main/java/com/example/spingchatbot/chat/service/ChatService.java
+++ src/main/java/com/example/spingchatbot/chat/service/ChatService.java
@@ -4,9 +4,14 @@ import com.example.spingchatbot.chat.exception.ChatException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.messages.AssistantMessage;
+import org.springframework.ai.chat.messages.Message;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.stereotype.Service;
 
-import java.util.Objects;
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @Service
@@ -15,16 +20,30 @@ public class ChatService {
 
     private final ChatClient chatClient;
 
+    private static final List<Message> chatHistory = new ArrayList<>();
+
     public String processMessage(final String messageFromUser) {
         try {
-            return Objects.requireNonNull(chatClient.prompt().user(messageFromUser).call()
-                            .chatResponse())
-                    .getResult()
-                    .getOutput()
-                    .getText();
+            addUserMessage(messageFromUser);
+            return getChatResponse();
         } catch (Exception e) {
             log.error(e.getMessage());
             throw new ChatException(e.getMessage());
         }
+    }
+
+    private String getChatResponse() {
+        ChatResponse chatResponse = chatClient.prompt().messages(chatHistory).call().chatResponse();
+        String textResponse = chatResponse.getResults().getFirst().getOutput().getText();
+        addAssistantResponse(textResponse);
+        return textResponse;
+    }
+
+    private void addUserMessage(String message) {
+        chatHistory.add(new UserMessage(message));
+    }
+
+    private void addAssistantResponse(String message) {
+        chatHistory.add(new AssistantMessage(message));
     }
 }
